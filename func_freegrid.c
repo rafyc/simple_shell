533dfa2d4008328a11b5bb62b8269fad5f6b657b
siham <phenissia2009@hotmail.fr>
2022-04-09 14:20:16 +0200

    correction betty style

--- func_freegrid.c
+++ func_freegrid.c
@@ -4,22 +4,20 @@
 
 /**
  * free_grid - main function
- * @grid: the address of the two dimensional grid
- * @height: height of the grid
- *
- * Return: Nothing.
+ * @grid: address of the two dimensional grid
+ * Return: void.
  */
 void free_grid(char **grid)
 {
-        int i = 0;
-        int height;
+	int i = 0;
+	int height;
 
-        while (grid[i])
-                i++;
+	while (grid[i])
+		i++;
 
-        height = i;
+	height = i;
 
-        for (i = 0; i < height; i++)
-                free(grid[i]);
-        free(grid);
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }

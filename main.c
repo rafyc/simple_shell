e5c22c150214fb00194a45f8471ebb2de8ba5c9c
gregcdjm <gregoirecoudrin@gmail.com>
2022-04-13 12:31:48 +0200

    e

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ int main(void)
 	int status = 1;
 	char *line;
 	char **args;
+	int i;
+	int j;
 
 	signal(SIGINT, ctrl_c);
 	while (status)
@@ -22,7 +24,22 @@ int main(void)
 		line = func_read();
 		if (line == NULL)
 			return (0);
-
+		i = 0;
+		j = 0;
+		while (line[j] != '\0')
+		{
+			if (line[i] == ' ')
+			{
+			i++;
+			}
+			j++;
+		}
+		printf("%d\n", i);
+		if (line[i] == '\0')
+		{
+		free(line);
+		continue;
+		}
 		if (_strcmp(line, "exit") == 0)
 		{
 			free(line);
